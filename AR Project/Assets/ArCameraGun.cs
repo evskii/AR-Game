ab7b030f45049cdf64dbcbaa3df136e17254941a
Evan Williams <evskimo97@gmail.com>
2020-11-08 12:18:36 +0000

    Worked on basic AR Functions.
    I worked on the basic AR functionality to see what we can achieve.
    
    I got the twerking zombie working (ARTtest_0.2.apk) and then a simple target that you can shoot (click on) that disapear. That means we have a script pretty much ready that allows palyers to click on shit in the AR world through their screens. (ARTest_0.25.apk)

---
+++ AR Project/Assets/ArCameraGun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArCameraGun : MonoBehaviour
+{
+    public Camera camera;
+
+    void Start() {
+        
+    }
+
+    private void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began) {
+                Shoot();
+            }
+        }
+    }
+
+    public void Shoot() {
+        RaycastHit hit;
+        Touch touch = Input.GetTouch(0);
+        Ray ray = camera.ScreenPointToRay(touch.position);
+
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.tag == "Target") {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
